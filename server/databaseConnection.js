2e3163ffabcb2589281aa77a3ccbd87792c20fdb
Ishan kadam <ishan@zataakse.com>
2025-10-23 23:18:02 +0530

    WIP

--- server/databaseConnection.js
+++ server/databaseConnection.js
@@ -14,6 +14,7 @@ const allowedOrigins = [
   "https://maieelace.onrender.com",
   "https://maieelace.vercel.app",
   "https://maieelace.netlify.app",
+  "https://maieelace.com",
 ];
 
 // ✅ CORS options with origin normalization and logging
